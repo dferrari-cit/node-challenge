333dbb1c5487ee9bd577d3c4d06cb3b5ac909aaf
CIANDT\caiosilva <caiosilva@ciandt.com>
2022-04-08 09:35:25 -0300

    Commit Swagger

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import express from 'express';
 import { router } from './router/router';
 import { errorMiddleware } from './middlewares/errorMiddleware';
+import SwaggerUI from 'swagger-ui-express';
+import swaggerDocs from "./swagger.json";
 const app = express();
 
 
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
+app.use('/api-docs',SwaggerUI.serve,SwaggerUI.setup(swaggerDocs));
 
 app.listen(4000, () => console.log("The server is runing"));
 
