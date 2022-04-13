f24e4178acd1a543df22fdeaa5f252e71ba5e5d1
Anthony Gonçalves de Oliveira Souza <anthonygdos.ciandt.com>
2022-04-13 11:38:31 -0300

    DN teste StarredDtomapper

---
+++ src/mapper/starred-dto.mapper.spec.ts
@@ -0,0 +1,20 @@
+import { StarredDto } from "../adapter/starred.dto";
+import { ResponseStarredMock } from "./mocks/response-starred.mock";
+import { StarredDtoMapper } from "./starred-dto.mapper";
+
+describe('StarredDtoMapper', () => {
+    let starredDtoMapper: StarredDtoMapper;
+    let responseStarredDto: Array<Object>;
+    let starredDto: StarredDto[];
+    beforeEach(() => {
+        starredDtoMapper = new StarredDtoMapper();
+        responseStarredDto = ResponseStarredMock;
+    });
+
+    describe('responseToDto', () => {
+        it('should return an array of StarredDto', () => {
+            starredDto = starredDtoMapper.responseToDto(responseStarredDto);
+            expect(starredDto[0]).toBeInstanceOf(StarredDto);
+        });
+    });
+});
