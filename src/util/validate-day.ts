88f87974a54f649af194e6399cb82b593ef3f595
Carlos Roberto Vilela Júnior <102747113+carlosjrCIT@users.noreply.github.com>
2022-05-04 15:32:11 -0300

    Update validate-day.ts

--- src/util/validate-day.ts
+++ src/util/validate-day.ts
@@ -1,5 +1,5 @@
 export function validateDay(text): string{
-    if(text > 3){
+    if(text.length > 3){
         const result = text.split('-',3);
         result[1] = result[1].length < 2 ? '0' + result[1]: result[1];
         result[1] = result.length == 2 ? result[1]+',' : result[1];
